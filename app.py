683d8300ff6bc8a927afd46d635908a6dcbc2610
Anastasiia <tagiltsevanastya@gmail.com>
2021-04-03 18:12:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 from dash.exceptions import PreventUpdate
-import gunicorn
+
 
 import numpy as np
 import pandas as pd
